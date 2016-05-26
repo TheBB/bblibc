034b5b179fd4ab328467365fe9c0d6910442876f
Eivind Fonn <evfonn@gmail.com>
2016-05-26 15:44:28 +0200

    Implement brk syscall

--- src/bbstdlib.h
+++ src/bbstdlib.h
@@ -1,8 +1,10 @@
 // Typedefs
-typedef unsigned long long size_t;
+typedef long unsigned int size_t;
+typedef long int ssize_t;
 
 // Functions implemented in asm
-void __sys_write(size_t fd, const char *s, size_t length);
+void __sys_write(int fd, const void *s, size_t length);
+size_t __sys_brk(void *addr);
 
 // Functions implemented in C
 void print(const char *s);
--- src/main.asm
+++ src/main.asm
@@ -2,9 +2,14 @@
 
 section .text
 global __sys_write
+global __sys_brk
 global _start
 __sys_write:
   mov rax, 1
+  syscall
+  ret
+__sys_brk:
+  mov rax, 12
   syscall
   ret
 _start:
